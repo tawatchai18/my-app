5d7646c4bed2f9f970b511ba3b1b6565ab4c2da5
account <tawatchai@codefin.io>
2018-10-22 10:22:22 +0700

    no message

--- src/App.js
+++ src/App.js
@@ -1,28 +1,87 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import { Input, Form, Button,Table,Layout,columns} from 'antd';
 
 class App extends Component {
+  constructor( ) {
+    super()
+    this.state = {
+      dataSource: []
+    }
+  }
+
+  componentDidMount() {
+
+    axios.get('https://ffc-nectec.herokuapp.com/v0/org?query=')  
+      .then(res => console.log(res)
+      )
+      .catch(function (error) {
+    }) 
+  }
+
   render() {
+    console.log(this.data);
+    
+    console.log(this.state.dataSource);
+
+    const columns = [{
+      title: 'NAME',
+      dataIndex: 'name',
+      key: 'name',
+      width: '2%'
+    },{
+      title: 'TEL',
+      dataIndex: 'tel',
+      key: 'tel',
+      width: '5%'
+    },{
+      title: 'ADDRESS',
+      dataIndex: 'address',
+      key: 'address',
+      width: '5%'
+    },{
+      title: 'ID',
+      dataIndex: 'id',
+      key: 'id',
+      width: '5%'
+    },{
+      title: 'TYPE',
+      dataIndex: 'type',
+      key: 'type',
+      width: '5%'
+    },{
+      title: 'TIMESTAMP',
+      dataIndex: 'timestamp',
+      key: 'timestamp',
+      width: '5%'
+    }]    
+
+    const Search = Input.Search;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div style={{marginTop:100}}>
+        <div className="App">
+          <div style={{marginTop:10}}>
+          </div>
+        </div >
+        <center>
+        <div style={{marginTop:50,width:600}}>  
+          <Search
+            placeholder="input search text"
+            enterButton="Search"
+            size="large"
+            onSearch={value => console.log(value)}
+          />
+        </div>  
+        <Layout.Content style = {{margin: '24px 16px 0', overflow: 'auto', display: 'flex', flexDirection: 'row'}}>
+          <div style = {{ padding: 24, background: '#fff', 'flex': 1, minHeight: '800px', minWidth: '800px', textAlign: 'left'}}>
+            <Table dataSource = {this.state.dataSource} columns = {columns}/>
+          </div>
+       </Layout.Content>
+        </center>
       </div>
     );
   }
 }
-
 export default App;
+
