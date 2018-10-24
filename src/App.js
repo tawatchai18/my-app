e7a173518b8f7d44c50428a67d4f380dbc10293c
account <tawat1838@gmail.com>
2018-10-24 11:19:10 +0700

    test api

--- src/App.js
+++ src/App.js
@@ -12,10 +12,10 @@ class App extends Component {
   }
 
   componentDidMount() {
-
-    axios.get('https://ffc-nectec.herokuapp.com/v0/org?query=')  
-      .then(res => console.log(res)
-      )
+    axios.get('https://api.sec.or.th/FundFactsheet/fund/amc/C0000000021', {
+      headers: {'Ocp-Apim-Subscription-Key' : '274a74499ac04ea7a23d4dd43ec680f4'}
+    }) 
+      .then(res => this.setState({dataSource: res.data}))
       .catch(function (error) {
     }) 
   }
@@ -25,37 +25,59 @@ class App extends Component {
     
     console.log(this.state.dataSource);
 
-    const columns = [{
-      title: 'NAME',
-      dataIndex: 'name',
-      key: 'name',
-      width: '2%'
-    },{
-      title: 'TEL',
-      dataIndex: 'tel',
-      key: 'tel',
-      width: '5%'
-    },{
-      title: 'ADDRESS',
-      dataIndex: 'address',
-      key: 'address',
-      width: '5%'
-    },{
-      title: 'ID',
-      dataIndex: 'id',
-      key: 'id',
-      width: '5%'
-    },{
-      title: 'TYPE',
-      dataIndex: 'type',
-      key: 'type',
-      width: '5%'
-    },{
-      title: 'TIMESTAMP',
-      dataIndex: 'timestamp',
-      key: 'timestamp',
-      width: '5%'
-    }]    
+    // const columns = [{
+    //   title: 'NAME',
+    //   dataIndex: 'name',
+    //   key: 'name',
+    //   width: '2%'
+    // },{
+    //   title: 'TEL',
+    //   dataIndex: 'tel',
+    //   key: 'tel',
+    //   width: '5%'
+    // },{
+    //   title: 'ADDRESS',
+    //   dataIndex: 'address',
+    //   key: 'address',
+    //   width: '5%'
+    // },{
+    //   title: 'ID',
+    //   dataIndex: 'id',
+    //   key: 'id',
+    //   width: '5%'
+    // },{
+    //   title: 'TYPE',
+    //   dataIndex: 'type',
+    //   key: 'type',
+    //   width: '5%'
+    // },{
+    //   title: 'TIMESTAMP',
+    //   dataIndex: 'timestamp',
+    //   key: 'timestamp',
+    //   width: '5%'
+    // }]    
+
+const columns = [{
+  title: 'ID',
+  dataIndex: 'regis_id',
+  key: 'regis_id',
+  width: '2%'
+},{
+  title: 'NameTh',
+  dataIndex: 'proj_name_th',
+  key: 'proj_name_th',
+  width: '5%'
+},{
+  title: 'NameEn',
+  dataIndex: 'proj_name_en',
+  key: 'proj_name_en',
+  width: '5%'
+},{
+  title: 'Date',
+  dataIndex: 'proj_abbr_name',
+  key: 'proj_abbr_name',
+  width: '2%'
+}]
 
     const Search = Input.Search;
     return (
